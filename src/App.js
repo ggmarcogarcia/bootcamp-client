ebb5f1a7dab916d3f8dde12eaf137750ab0dab7c
Garcia <Marco.Garcia@us.sogeti.com>
2020-01-29 14:32:23 -0500

    employee info react

--- src/App.js
+++ src/App.js
@@ -1,26 +1,54 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      employees: []
+    }
+  }
+
+  componentDidMount() {
+    fetch('http://localhost:3005/getSogetiEmployees')    //fetch for API calls
+      .then(employees => employees.json())
+      .then(employees => {
+        console.log(employees)
+        this.setState({
+          employees: employees
+        })
+      })
+      .catch(err => console.log(err))
+  }
+
+  render() {
+    return (
+      <div className="App container">
+        <h1>Sogeti Employee Directory </h1>
+        < div className="row">
+          {this.state.employees.map((employee, index) => {
+            return (
+              <div class="media col-12 mb-4 p-3 directory-entry">
+                <img id="cat-directory" class="mr-3" alt="..." src={employee.image} />
+                <div class="media-body">
+                  <h5 class="mt-0">{employee.name} </h5>
+                 <h4> {employee.location}</h4>
+                  <h5>{employee.number}</h5>
+                </div>
+                <img id="city" src={employee.city}/>
+               <h7> <button> Things to do in {employee.location} </button></h7>
+              </div>
+            )
+          })
+          }
+        </ div>
+      </div>
+    );
+  }
 }
 
 export default App;
